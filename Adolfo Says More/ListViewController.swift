b7d2b2d861fd75e75f8fd881dc6eea3dcf38072d
gunzalez <gunzalez@hotmail.com>
2015-07-02 20:42:06 +0100

    Changes list screen to UItableViewController class and nib

--- Adolfo Says More/ListViewController.swift
+++ Adolfo Says More/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  ListViewController.swift
 //  Adolfo Says More
 //
 //  Created by Segun Konibire on 02/07/2015.
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,136 @@ class ListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return adolfoMeans.count
+    
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = adolfoMeans[indexPath.row][0]
+        
+        if adolfoMeans[indexPath.row][1] == "yes" {
+            
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            
+        } else {
+            
+            cell.accessoryType = UITableViewCellAccessoryType.None
+            
+        }
+        
+        return cell
+        
+    }
+    
+//    
+//    override func viewDidAppear(animated: Bool) {
+//        
+//        if adolfoMeans.count >= maxMeaning {
+//            
+//            self.newBarButton.enabled = false
+//            
+//        } else {
+//            
+//            self.newBarButton.enabled = true
+//            
+//        }
+//        
+//        tableList.reloadData()
+//        
+//    }
+    
+    
+//    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//        
+//        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+//            
+//            adolfoMeans.removeAtIndex(indexPath.row)
+//            
+//            NSUserDefaults.standardUserDefaults().setObject(adolfoMeans, forKey: "adolfoMeans");
+//            
+//            if adolfoMeans.count >= maxMeaning {
+//                
+//                self.newBarButton.enabled = false
+//                
+//            } else {
+//                
+//                self.newBarButton.enabled = true
+//                
+//            }
+//            
+//            tableList.reloadData()
+//            
+//        }
+//    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
