db723fbedfbc7bdb5cf74558362e3f5cd74c1da3
gunzalez <gunzalez@hotmail.com>
2015-07-02 20:00:00 +0100

    Hooks up card view controller, minor layout tweaks

--- Adolfo Says More/CardViewController.swift
+++ Adolfo Says More/CardViewController.swift
@@ -12,35 +12,30 @@ class CardViewController: UIViewController {
     
     var card: String!
     
-//    var hideOverride = "Need to find a way to select override adolfo meaning"
-//    
-//    var tempMeaning: String!
-//    
-//    @IBOutlet var cardLabel: UILabel!
-//    
-//    @IBOutlet var meansLabel: UILabel!
-//    
-//    @IBOutlet var hideButton: UIBarButtonItem!
-//    
-//    @IBAction func hideButtonPressed(sender: AnyObject) {
-//        
-//        if hideButton.title == "Hide" {
-//            
-//            meansLabel.text = replaceStoryPoints(hideOverride)
-//            
-//            hideButton.title = "Unhide"
-//            
-//            
-//        } else {
-//            
-//            meansLabel.text = replaceStoryPoints(tempMeaning)
-//            
-//            hideButton.title = "Hide"
-//            
-//        }
-//        
-//    }
+    var hideOverride = "Need to find a way to select override adolfo meaning"
     
+    var tempMeaning: String!
+    
+    var isCloaked: Bool = false
+    
+    @IBOutlet var cardLabel: UILabel!
+    
+    @IBOutlet var meansLabel: UILabel!
+    
+    @IBAction func bailOutButtonPressed(sender: AnyObject) {
+        
+        isCloaked = !isCloaked
+        
+        if isCloaked {
+            
+            meansLabel.text = replaceStoryPoints(hideOverride)
+            
+        } else {
+            
+            meansLabel.text = replaceStoryPoints(tempMeaning)
+        }
+        
+    }
     
     
     @IBAction func donePressed(sender: AnyObject) {
@@ -65,41 +60,41 @@ class CardViewController: UIViewController {
         super.viewDidLoad()
         
 //        modalTransitionStyle = .FlipHorizontal
-//        
-//        cardLabel.text = card
-//        
-//        var favourites = [Array<String>]()
-//        
-//        if adolfoMeans.count > 0 {
-//            
-//            for singleMeaning in adolfoMeans {
-//                
-//                if singleMeaning[1] == "yes" {
-//                    
-//                    favourites.append(singleMeaning)
-//                    
-//                }
-//                
-//            }
-//            
-//            if favourites.isEmpty {
-//                
-//                favourites = adolfoMeans
-//                
-//            }
-//            
-//            var randomNumber = Int(arc4random_uniform(UInt32(favourites.count)))
-//            
-//            meansLabel.text = replaceStoryPoints(favourites[randomNumber][0])
-//            
-//            tempMeaning = favourites[randomNumber][0]
-//            
-//            
-//        } else {
-//            
-//            meansLabel.text = replaceStoryPoints(defaultMeans)
-//            
-//        }
+   
+        cardLabel.text = card
+        
+        var favourites = [Array<String>]()
+        
+        if adolfoMeans.count > 0 {
+            
+            for singleMeaning in adolfoMeans {
+                
+                if singleMeaning[1] == "yes" {
+                    
+                    favourites.append(singleMeaning)
+                    
+                }
+                
+            }
+            
+            if favourites.isEmpty {
+                
+                favourites = adolfoMeans
+                
+            }
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(favourites.count)))
+            
+            meansLabel.text = replaceStoryPoints(favourites[randomNumber][0])
+            
+            tempMeaning = favourites[randomNumber][0]
+            
+            
+        } else {
+            
+            meansLabel.text = replaceStoryPoints(defaultMeans)
+            
+        }
         
     }
 
